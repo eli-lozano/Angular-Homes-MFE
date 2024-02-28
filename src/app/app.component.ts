aa708b38f99f19bfc65fd405eac7e8f3183bc43c
Eli Lozano <elijah.lozano@care.com>
2024-02-28 15:31:58 -0600

    feat: bootstrap homes app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent]
 })
 export class AppComponent {
   title = 'homes';
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
+import { MockListings } from 'src/assets/mocks/listing-mocks';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = MockListings;
+}
